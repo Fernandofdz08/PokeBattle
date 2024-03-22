b3ccec71e2e68b37f615d6c6a89f142e672463a9
Fernandofdz08 <fernando.fernandezo@outlook.com>
2024-03-22 21:49:09 +0000

    Se añader la ruta y la vista para poder realizar la lectura de uno de los archivos csv.

--- PokeBattle/app_pokebattle/urls.py
+++ PokeBattle/app_pokebattle/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('prueba/', views.prueba, name='prueba_django')
+    path('prueba/', views.prueba, name='prueba_django'), 
+    path('pokemon_csv', views.pokemon_csv, name='pokemon_csv'),
 ]
--- PokeBattle/app_pokebattle/views.py
+++ PokeBattle/app_pokebattle/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os, csv, pandas as pd
 
 # Create your views here.
 
 def prueba(request):
-    a = 5
+    a = 5 
     b = 3
     c = a + b
     print(c)
     return HttpResponse(f"La suma de {a} + {b} es igual a {c}")
+
+def pokemon_csv(request):
+    # Sabemos que estamos en directorio app_pokebattle
+    # Nuestro archivo csv está en la carpeta raiz del proyecto
+    # Vamos a obtener la ruta del archivo csv
+    # Sabemos que esta dos directorios atrás, por lo que
+    # la ruta sería ../../nombre_archivo.csv
+
+    # Obtenemos la ruta del archivo csv
+    csv_path = os.path.abspath(os.path.join(os.getcwd(), "../Pokemon - Especiales.csv"))
+
+    # Abrimos el archivo csv en modo lectura
+
+    with open(csv_path, "r") as pokemon_csv:
+        # Con el modulo pandas, leemos el archivo. 
+        #Esto devuelve una lista, que contiene un elemento por cada fila leida
+        df = pd.read_csv(pokemon_csv)
+        print(df)
+        return HttpResponse(df.to_html())
+        
