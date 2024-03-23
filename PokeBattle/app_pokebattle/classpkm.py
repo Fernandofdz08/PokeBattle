2162a1f9efee7c8021035952f7c5db06387bfaf2
Cturico <benjuturon01@gmail.com>
2024-03-23 17:34:51 +0100

    ++

--- PokeBattle/app_pokebattle/classpkm.py
+++ PokeBattle/app_pokebattle/classpkm.py
@@ -1,7 +1,23 @@
 class Pokemon:
-    def __init__(self, name : str, type1 : str, type2 : str, Ps : int, Atck : int, Def : int, AtckSp : int, DefSp : int, Vel : int, Hability : str, Object : str, At1 : str, TypeAt1 : str, At2 : str, TypeAt2 : str, At3 : str, TypeAt3 : str, At4 : str, TypeAt4 : str, Teratype : str, Is_Teratype : bool):
+    def __init__(self, name : str, type1 : str, type2 : str, Ps : int, At : int, Def : int, AtSp : int, DefSp : int, Vel : int, Hability : str, Object : str, At1 : str, TypeAt1 : str, At2 : str, TypeAt2 : str, At3 : str, TypeAt3 : str, At4 : str, TypeAt4 : str, Teratype : str, Is_Teratype : bool):
         self.name = name
         self.type1 = type1
         self.type2 = type2
         self.Ps = Ps
-        self.Atck = Atck
+        self.At = At
+        self.Def = Def
+        self.AtSp = AtSp
+        self.DefSp = DefSp
+        self.Vel = Vel
+        self.Hability = Hability
+        self.Object = Object
+        self.At1 = At1
+        self.TypeAt1 = TypeAt1
+        self.At2 = At2
+        self.TypeAt2 = TypeAt2
+        self.At3 = At3
+        self.TypeAt3 = TypeAt3
+        self.At4 = At4
+        self.TypeAt4 = TypeAt4
+        self.Is_Teratype = Is_Teratype
+        
